6d4bd7114eb332db2c4ef360bdb28b9d8b602029
Yosua Tarigan <yosuatarigan7@gmail.com>
2020-05-05 23:55:59 +0700

    Updates

---
+++ precache-manifest.ca79fdf3ff290b76ae5a4bba45aa4915.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c7cbc05a91d1b027982af213aaea33d",
+    "url": "/pkbm2/index.html"
+  },
+  {
+    "revision": "94b9f81f1803a53c0254",
+    "url": "/pkbm2/static/css/2.d6351aec.chunk.css"
+  },
+  {
+    "revision": "3e0d9307fc817718a421",
+    "url": "/pkbm2/static/css/main.20dddb12.chunk.css"
+  },
+  {
+    "revision": "94b9f81f1803a53c0254",
+    "url": "/pkbm2/static/js/2.6ec66c85.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/pkbm2/static/js/2.6ec66c85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e0d9307fc817718a421",
+    "url": "/pkbm2/static/js/main.e4f053e9.chunk.js"
+  },
+  {
+    "revision": "f2a6256d3bc57b222639",
+    "url": "/pkbm2/static/js/runtime-main.a59b6f60.js"
+  },
+  {
+    "revision": "9ed58d756fbfac64d9c2aea47c08eb74",
+    "url": "/pkbm2/static/media/1.9ed58d75.jpg"
+  },
+  {
+    "revision": "bef41825248b4e4c20a5429c1c65f159",
+    "url": "/pkbm2/static/media/2.bef41825.jpg"
+  },
+  {
+    "revision": "e7ec8dbea9ed8aa51438804d4de0023b",
+    "url": "/pkbm2/static/media/3.e7ec8dbe.jpg"
+  },
+  {
+    "revision": "015700d4aa317a07a73fc84cc43f0fb4",
+    "url": "/pkbm2/static/media/question.015700d4.png"
+  },
+  {
+    "revision": "a6a8aeb01a6d77dc064269f5aaa468ea",
+    "url": "/pkbm2/static/media/teacher.a6a8aeb0.png"
+  },
+  {
+    "revision": "a8529dac7a86dfdc9ca620fbefb733f4",
+    "url": "/pkbm2/static/media/tentor.a8529dac.jpg"
+  },
+  {
+    "revision": "36794b86035707c44ea5adbba0604503",
+    "url": "/pkbm2/static/media/topiwisuda.36794b86.png"
+  }
+]);
